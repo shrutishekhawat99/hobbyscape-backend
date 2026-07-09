0609dceb0b74898e29db5ade16d9b908bfc8ce01
shrutishekhawat99 <shrutishekhawat1999@gmail.com>
2026-07-09 17:07:43 +0530

    Delete src directory

--- src/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAllProducts } = require("../controllers/productController");
-
-router.get("/", getAllProducts);
-
-module.exports = router;
